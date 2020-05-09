495533df7ad77b362bc9d0b2e738f2b060e5b9fc
Marco Toranzo <adm@MacBook-Pro-de-Adm.local>
2020-05-09 06:25:47 -0700

    creandolos modelos, controladores y migraciones

---
+++ app/Http/Controllers/MemorandumController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\models\Memorandum;
+use Illuminate\Http\Request;
+
+class MemorandumController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\models\Memorandum  $memorandum
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Memorandum $memorandum)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\models\Memorandum  $memorandum
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Memorandum $memorandum)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\models\Memorandum  $memorandum
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Memorandum $memorandum)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\models\Memorandum  $memorandum
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Memorandum $memorandum)
+    {
+        //
+    }
+}
---
+++ database/migrations/2020_05_09_115209_create_memoranda_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMemorandaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('memoranda', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('memoranda');
+    }
+}
